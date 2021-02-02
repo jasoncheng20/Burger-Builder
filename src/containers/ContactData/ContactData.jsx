c2d8425f458a09b9b6ad2ed8d4d340940874c3fc
Jason Cheng <jasoncheng20@gmail.com>
2021-02-02 11:58:50 -0800

    add two-way binding to form

--- src/containers/ContactData/ContactData.jsx
+++ src/containers/ContactData/ContactData.jsx
@@ -4,14 +4,69 @@ import classes from "./ContactData.module.css";
 
 import Button from "../../components/UI/Button";
 import Spinner from "../../components/UI/Spinner";
+import Input from "../../components/UI/Input";
 
 class ContactData extends Component {
   state = {
-    name: "",
-    email: "",
-    address: {
-      street: "",
-      postalCode: "",
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Name",
+        },
+        value: "",
+      },
+      address1: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Address Line 1",
+        },
+        value: "",
+      },
+      address2: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Address Line 2",
+        },
+        value: "",
+      },
+      state: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "State",
+        },
+        value: "",
+      },
+      zip: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Zip Code",
+        },
+        value: "",
+      },
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Email Address",
+        },
+        value: "",
+      },
+      deliveryMethod: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            { value: "fastest", displayValue: "Fastest" },
+            { value: "cheapest", displayValue: "Cheapest" },
+          ],
+        },
+        value: "---",
+      },
     },
     loading: false,
   };
@@ -34,20 +89,42 @@ class ContactData extends Component {
       .post("/orders.json", order)
       .then((response) => {
         this.setState({ loading: false });
-        this.props.history.push('/')
+        this.props.history.push("/");
       })
       .catch((error) => {
         this.setState({ loading: false });
       });
   };
 
+  inputChangedHander = (event, formfield) => {
+    // create clones
+    const updatedOrderForm = { ...this.state.orderForm };
+    const updatedFormField = { ...updatedOrderForm[formfield] };
+    // update clones without mutating original
+    updatedFormField.value = event.target.value;
+    updatedOrderForm[formfield] = updatedFormField;
+    //set state
+    this.setState({ orderForm: updatedOrderForm });
+  };
+
   render() {
+    const formfields = [];
+    const { orderForm } = this.state;
+    for (let field in orderForm) {
+      formfields.push(field);
+    }
+
     let form = (
       <form>
-        <input type="text" name="name" placeholder="your name" />
-        <input type="email" name="email" placeholder="your email" />
-        <input type="text" name="street" placeholder="your street" />
-        <input type="text" name="postal" placeholder="postal code" />
+        {formfields.map((field) => (
+          <Input
+            key={field}
+            elementType={orderForm[field].elementType}
+            elementConfig={orderForm[field].elementConfig}
+            value={orderForm[field].value}
+            changed={(event) => this.inputChangedHander(event, field)}
+          />
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>
           PLACE YOUR ORDER
         </Button>
